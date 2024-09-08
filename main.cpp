0ed3edef175ddfe593c5e1276dee82cd060c34a8
Hightree <166693367+hightree-dev@users.noreply.github.com>
2024-09-08 18:04:29 +0900

    틱 추가

--- main.cpp
+++ main.cpp
@@ -14,12 +14,19 @@ int figure[4] = { 0, 1, 3, 5};
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(800, 600), "Tetris");
+  sf::Clock clock;
 
   Board board(10, 20);
   Block block(figure, sf::Color::Green);
 
+  float timer = 0.f;
+  float delay = 0.5f;
+
   while (window.isOpen())
   {
+    timer += clock.getElapsedTime().asSeconds();
+    clock.restart();
+
     sf::Event event;
     while (window.pollEvent(event))
     {
@@ -32,13 +39,20 @@ int main()
         if (event.key.code == sf::Keyboard::Left) block.move({-1.f, 0.f});
         else if (event.key.code == sf::Keyboard::Right) block.move({1.f, 0.f});
         else if (event.key.code == sf::Keyboard::Up) block.rotate();
-        else if (event.key.code == sf::Keyboard::Down) block.move({0.f, 1.f});
         if (board.isCollision(block.getPosisitons()))
           block.back();
         break;
       default:
         break;
       }
+    }
+
+    if (timer > delay)
+    {
+      block.move({0.f, 1.f});
+      if (board.isCollision(block.getPosisitons()))
+          block.back();
+      timer = 0.f;
     }
     
     window.clear(sf::Color::White);
