2bfb1ac16893449d465105c9ccdc7a2ab66a19ee
hightree <dev.hightree@gmail.com>
2024-09-24 01:46:37 +0900

    멀티 스레드 에코 테트리스 서버

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -1,47 +1,28 @@
 #include <iostream>
 #include <SFML/Network.hpp>
-#include <SFML/Graphics.hpp>
-
-#include "Board.hpp"
-#include "Tetromino.hpp"
 
 int main()
 {
-  sf::RenderWindow window(sf::VideoMode(800, 600), "Server");
+  sf::TcpListener listener;
+  listener.listen(8000);
+  
+  std::vector<std::unique_ptr<sf::Thread>> threads;
 
-  Board board(20, 10, sf::Color::Black);
-  Tetromino tetromino({0, 0, 0, 0}, sf::Color::Transparent);
-
-  std::unique_ptr<sf::Thread> thread = std::make_unique<sf::Thread>([&board, &tetromino]{
-    sf::TcpListener listener;
-    listener.listen(8000);
+  while (true) {
+    auto socket = std::make_shared<sf::TcpSocket>();
+    listener.accept(*socket);
     
-    sf::TcpSocket socket;
-    listener.accept(socket);
-   
     std::cout << "connected" << std::endl;
 
-    while (true) {
-      sf::Packet packet;
-      socket.receive(packet);
-      packet >> board;
-      packet >> tetromino;
-      socket.send(packet);
-    }
-  });
+    auto thread = std::make_unique<sf::Thread>([socket](){
+      while (true) {
+        sf::Packet packet;
+        socket->receive(packet);
+        socket->send(packet);
+      }
+    });
+    thread->launch();
 
-  thread->launch();
-
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event))
-      if (event.type == sf::Event::Closed)
-        window.close();
-    window.clear(sf::Color::White);
-    board.render(window, sf::Vector2f(0.f, 0.f), 20.f);
-    tetromino.render(window);
-    window.display();
+    threads.emplace_back(std::move(thread));
   }
-
-  
 }
