2fabdef160523a1e0d4ce6f019a4836f6d4fa81c
hightree <dev.hightree@gmail.com>
2024-09-25 01:34:42 +0900

    Select 에코 테트리스 서버

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -1,26 +1,54 @@
 #include <iostream>
+#include <list>
 #include <SFML/Network.hpp>
 
 int main()
 {
-  sf::TcpListener listener;
-  listener.listen(8000);
-  listener.setBlocking(false);
+  sf::SocketSelector selector;
 
-  std::vector<std::unique_ptr<sf::TcpSocket>> sockets;
+  sf::TcpListener listener;
+  if (listener.listen(8000) != sf::Socket::Done) 
+    return 1;
+  selector.add(listener);
+
+  // 과연 랜덤 액세스를 할 일이 있을까?
+  // 물론 빡세게 고려하지 않아도 됨. 왜냐하면 stl 컨테이너라 컨테이너만 갈아끼우면 되기 때문임.
+  // 소켓 컨테이너는 랜덤 액세스보다 연결 끊어져서 erase 하는 케이스가 더 많을 듯 함.
+  std::list<std::unique_ptr<sf::TcpSocket>> clients;
 
   while (true) {
-    auto socket = std::make_unique<sf::TcpSocket>();
-    socket->setBlocking(false);
-    if (listener.accept(*socket) == sf::Socket::Done) {
-      std::cout << "connected" << std::endl;
-      sockets.push_back(std::move(socket));
+    if (!selector.wait())
+      continue;
+    
+    if (selector.isReady(listener)) {
+      std::unique_ptr<sf::TcpSocket> client = std::make_unique<sf::TcpSocket>();
+      if (listener.accept(*client) == sf::Socket::Done) {
+        std::cout << "connected" << std::endl;
+        selector.add(*client);
+        clients.push_back(std::move(client));
+      }
     }
 
-    for (auto& socket : sockets) {
+    for (auto it = clients.begin(); it != clients.end(); ++it) {
+      auto& client = **it;
+      if (!selector.isReady(client)) 
+        continue;
+      
       sf::Packet packet;
-      if (socket->receive(packet) == sf::Socket::Done)
-        socket->send(packet);
+      auto status = client.receive(packet);
+      
+      if (status == sf::Socket::Done) {
+        auto status = client.send(packet);
+        if (status == sf::Socket::Disconnected) {
+          selector.remove(client);
+          client.disconnect();
+          it = clients.erase(it);  
+        }
+      } else if (status == sf::Socket::Disconnected) {
+        selector.remove(client);
+        client.disconnect();
+        it = clients.erase(it);
+      }
     }
   }
 }
