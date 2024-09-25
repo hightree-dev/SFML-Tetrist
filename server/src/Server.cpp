14b212ce3e03a49d2e5f15d477d108266c411b60
hightree <dev.hightree@gmail.com>
2024-09-25 23:40:04 +0900

    셀렉트 서버

--- server/src/Server.cpp
+++ server/src/Server.cpp
@@ -3,28 +3,34 @@
 
 int main()
 {
+  sf::SocketSelector selector;
   sf::TcpListener listener;
   listener.listen(8000);
-  listener.setBlocking(false);
+  
+  selector.add(listener);
 
   std::vector<std::unique_ptr<sf::TcpSocket>> sockets;
 
   while (true) {
-    if (sockets.size() < 2) {
+    if (!selector.wait())
+      continue;
+
+    if (sockets.size() < 2 && selector.isReady(listener)) {
       auto socket = std::make_unique<sf::TcpSocket>();
-      socket->setBlocking(false);
       if (listener.accept(*socket) == sf::Socket::Done) {
-        std::cout << "connected" << std::endl;
+        selector.add(*socket);
+        socket->setBlocking(false);
         sockets.push_back(std::move(socket));
+        std::cout << "connected " << sockets.size() << std::endl;
       }
-    }
-    else {
-      {
+    } else if(sockets.size() == 2){
+      if (selector.isReady(*sockets[0])) {
         sf::Packet packet;
         if (sockets[0]->receive(packet) == sf::Socket::Done)
           sockets[1]->send(packet);
       }
-      {
+
+      if (selector.isReady(*sockets[1])){
         sf::Packet packet;
         if (sockets[1]->receive(packet) == sf::Socket::Done)
           sockets[0]->send(packet);
